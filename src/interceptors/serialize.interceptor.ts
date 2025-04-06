3debc865fba2ec7bc1bafe38d288a654724698a9
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2025-04-06 21:59:40 +0100

    update

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,17 +7,29 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
+
+interface ClassConstructor {
+    new (...args: any[]): {}
+}
+export function Serialize(dto: ClassConstructor) {
+    return UseInterceptors(new SerializeInterceptor(dto))
+}
 
 export class SerializeInterceptor implements NestInterceptor {
+   constructor(private dto: any) {}
+   
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
         
-
         console.log('I am running before the handler', context);
 
         return handler.handle().pipe(
             map((data: any) => {
               // Run something  
               console.log('Running before response is sent out', data);
+                return plainToClass(UserDto, data, {
+                   excludeExtraneousValues: true,  
+                })
             })
         )
     }
