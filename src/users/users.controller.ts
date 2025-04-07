dc2f47bf151ed2346e9ea6de3ab35a9889144836
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2025-04-07 15:14:48 +0100

    user update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException, Session, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from '../users/dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+// import { Session } from 'inspector/promises';
 
 
 @Controller('auth')
@@ -15,16 +16,33 @@ export class UsersController {
         private authService: AuthService
     ) {}
 
+    @Get('/getme')
+    getMe(@Session() session: any) {
+        const user = this.userService.findOne(session.userId);
+        if (!user) {
+            throw new NotFoundException('kindly sign in')
+        }
+        return user;
+    }
+
+    
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
+    }
+
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
-        console.log(body);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     @Post('/signin')
-    signin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
-        
+    async signin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
     }
 
     
@@ -51,6 +69,16 @@ export class UsersController {
     @Patch('/:id')
     updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.userService.update(parseInt(id), body);
+    }
+
+    @Get('/colors/:color')
+    setColor(@Param('color') color: string, @Session() session: any) {
+        session.color = color;
+    }
+
+    @Get('/colors/')
+    getColor(@Session() session: any) {
+        return session.color;
     }
 
 
