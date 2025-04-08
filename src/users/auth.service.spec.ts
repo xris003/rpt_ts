af9a1a4f43abde818e63f88b0a1906a61468676a
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2025-04-08 22:52:49 +0100

    test integration

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,59 @@
+import { Test } from "@nestjs/testing";
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { AuthService } from "./auth.service";
+import { UsersService } from "./users.service";
+import { User } from "./user.entity";
+
+describe('AuthService', () => {
+    let service: AuthService;
+    let fakeUsersService: Partial<UsersService>
+
+    beforeEach(async () => {
+ // Create a fake copy of the users service
+     fakeUsersService = {
+        find: () => Promise.resolve([]),
+        create: (email: string, password: string) => 
+         Promise.resolve({id:1, email, password} as User)
+   };
+    const module = await Test.createTestingModule ({
+        providers: [
+            AuthService,
+        {
+            provide: UsersService,
+            useValue: fakeUsersService
+        }]
+
+    }).compile();
+
+     service = module.get(AuthService);
+})
+
+    it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+    });
+
+    it('creates a new user with a salted and hashed password', async() => {
+        const user = await service.signup('fideloti@gmail.com', 'password1234');
+
+        expect(user.password).not.toEqual('password1234');
+        const [salt, hash] = user.password.split('.');
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
+    })
+
+    it('throws an error if user signs up with email that is in use', async () => {
+        fakeUsersService.find = () =>
+    
+          Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+          BadRequestException,
+        );
+      });
+
+    it('throws an error if sigin is called with an used email', async () => {
+        await expect(
+            service.signin('asdf@asdf.com', 'passasdf'),
+        ).rejects.toThrow(NotFoundException);
+    });
+
+});
