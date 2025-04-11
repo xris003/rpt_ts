744844387f2b1b92a3c27a90082cdbd39448322a
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2025-04-11 01:20:54 +0100

    user test update

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -24,7 +24,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {}
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password} as  User)
+      }
     };
 
 
@@ -65,6 +67,16 @@ describe('UsersController', () => {
     fakeUsersService.findOne = () => null;
     await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
   });
+
+  it('signin updates session object and returns user', async () => {
+    const session = {userId: -10 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'asdf'},
+      session,
+  );
+
+  expect(user.id).toEqual(1);
+  expect(session.userId).toEqual(1)
 });
 
-
+})
